8375e149df0b6f137519958f528f4fce476a86b8
Jean-Pierre Thach <jeanpierrethach@gmail.com>
2019-12-24 11:05:10 -0500

    Added reviews extraction

---
+++ src/extract_reviews.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import ast
+import argparse
+
+from utils import maybe_make_directory
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str,
+                        default='../data/australian_user_reviews.json',
+                        help='Directory path to the data. (required) (default: %(default)s)')
+    parser.add_argument('--output_dir', type=str,
+                        default='reviews',
+                        help='Directory path to the reviews. (default: %(default)s)')
+    
+    args = parser.parse_args()
+    
+    maybe_make_directory(args.output_dir)
+
+    return args
+
+args = parse_args()
+
+review_output_path = os.path.join(args.output_dir, "reviews.txt")
+
+with open (review_output_path, 'w') as output_file:
+    with open(args.data_path) as json_file:
+        for line in json_file:
+            data_dict = ast.literal_eval(line)
+            for review in data_dict['reviews']:
+                output_file.write(review['review'] + "\n")
